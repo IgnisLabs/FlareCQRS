557d05bd85f90aa598c496748e418c34c1365098
Luciano Longo <luciano.longo@gmail.com>
2017-01-17 23:33:42 -0300

    Add missing docblock

--- src/Query/QueryBus.php
+++ src/Query/QueryBus.php
@@ -6,6 +6,11 @@ use IgnisLabs\FlareCQRS\MessageBus;
 
 class QueryBus extends MessageBus {
 
+    /**
+     * Dispatch the query to it's handler
+     * @param $query
+     * @return Result
+     */
     public function dispatch($query) : Result {
         return new Result($this->handle($query));
     }
