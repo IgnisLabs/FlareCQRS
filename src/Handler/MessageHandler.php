34140484f45a8965a6efb06b8adbc5048c443030
Luciano Longo <luciano.longo@gmail.com>
2017-01-21 01:12:57 -0300

    Add missing dockbloc for MessageHandler interface

--- src/Handler/MessageHandler.php
+++ src/Handler/MessageHandler.php
@@ -3,5 +3,10 @@
 namespace IgnisLabs\FlareCQRS\Handler;
 
 interface MessageHandler {
+    /**
+     * Handle a message
+     * @param object $message
+     * @return mixed
+     */
     public function handle($message);
 }
